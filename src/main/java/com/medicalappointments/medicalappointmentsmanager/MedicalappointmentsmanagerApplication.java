4b63ace31918d85e9f3e8916343c57e41ed798b7
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-16 11:28:13 -0400

    first commit

---
+++ src/main/java/com/medicalappointments/medicalappointmentsmanager/MedicalappointmentsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.medicalappointments.medicalappointmentsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalappointmentsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalappointmentsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicalappointments/medicalappointmentsmanager/MedicalappointmentsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalappointments.medicalappointmentsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalappointmentsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
