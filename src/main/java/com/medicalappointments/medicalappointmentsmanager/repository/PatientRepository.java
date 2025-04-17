1b86e89f611ae8423027fceacc80ea5793543116
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-17 16:57:35 -0400

    Add findByEmail method to PatientRepository for email lookup

--- src/main/java/com/medicalappointments/medicalappointmentsmanager/repository/PatientRepository.java
+++ src/main/java/com/medicalappointments/medicalappointmentsmanager/repository/PatientRepository.java
@@ -2,9 +2,10 @@
 package com.medicalappointments.medicalappointmentsmanager.repository;
 
 import com.medicalappointments.medicalappointmentsmanager.models.Patient;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, UUID>{
-    
+    Optional<Patient> findByEmail(String email);
 }
