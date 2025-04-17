28073c6a87e594f032a1c7f11715a16e2f937390
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-17 18:32:20 -0400

    Refactor DoctorRepository to return a list of doctors by specialty instead of an optional doctor

--- src/main/java/com/medicalappointments/medicalappointmentsmanager/repository/DoctorRepository.java
+++ src/main/java/com/medicalappointments/medicalappointmentsmanager/repository/DoctorRepository.java
@@ -1,11 +1,13 @@
 
 package com.medicalappointments.medicalappointmentsmanager.repository;
 
+import com.medicalappointments.medicalappointmentsmanager.enums.DoctorSpecialty;
 import com.medicalappointments.medicalappointmentsmanager.models.Doctor;
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<Doctor, UUID>{
-    Optional<Doctor> findBySpecialty(String specialty);
+    List<Doctor> findBySpecialty(DoctorSpecialty specialty);
+
 }
