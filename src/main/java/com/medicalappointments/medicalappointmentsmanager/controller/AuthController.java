a249b821d56eb67405e2a8263eab6e7ccacca395
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-17 16:57:10 -0400

    Refactor AuthController to include PatientService and improve registration flow

--- src/main/java/com/medicalappointments/medicalappointmentsmanager/controller/AuthController.java
+++ src/main/java/com/medicalappointments/medicalappointmentsmanager/controller/AuthController.java
@@ -2,7 +2,9 @@
 package com.medicalappointments.medicalappointmentsmanager.controller;
 
 import com.medicalappointments.medicalappointmentsmanager.dtos.request.PatientRequest;
+import com.medicalappointments.medicalappointmentsmanager.services.PatientService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +14,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+    
+    @Autowired
+    private PatientService patientService;
+    
+    @GetMapping("/")
+    public String loginPage(Model model){
+        model.addAttribute("patient", new PatientRequest());
+        return "login";
+    }
+    
     @GetMapping("/register")
     public String registerPage(Model model){
         model.addAttribute("patient", new PatientRequest());
@@ -21,11 +33,15 @@ public class AuthController {
     @PostMapping("/register")
     public String processRegisterForm(
             @Valid @ModelAttribute("patient") PatientRequest patient,
-            BindingResult result){
+            BindingResult result,
+            Model model){
         if(result.hasErrors()){
             return "register";
         }
+              
+        patientService.savePatient(patient);
+        model.addAttribute("patient", patient);
         
-        return "relax";
+        return "login";
     }
 }
