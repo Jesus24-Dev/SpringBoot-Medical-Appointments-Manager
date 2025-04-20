f6671d2e9672b66c5ead5a5e1f5db836874874b0
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-20 18:26:22 -0400

    Add method to check appointment existence by doctor, date, and time for improved validation

--- src/main/java/com/medicalappointments/medicalappointmentsmanager/repository/AppointmentRepository.java
+++ src/main/java/com/medicalappointments/medicalappointmentsmanager/repository/AppointmentRepository.java
@@ -2,8 +2,11 @@
 package com.medicalappointments.medicalappointmentsmanager.repository;
 
 import com.medicalappointments.medicalappointmentsmanager.models.Appointment;
+import com.medicalappointments.medicalappointmentsmanager.models.Doctor;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
-    
+    boolean existsByDoctorAndAppointmentDateAndAppointmentTime(Doctor doctor, LocalDate appointmentDate, LocalTime appointmentTime);
 }
