9b8a82ccd46d9356de143117a718377c5c365dca
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-20 18:25:40 -0400

    Add shiftType to DoctorMinResponse for enhanced doctor details

--- src/main/java/com/medicalappointments/medicalappointmentsmanager/dtos/response/DoctorMinResponse.java
+++ src/main/java/com/medicalappointments/medicalappointmentsmanager/dtos/response/DoctorMinResponse.java
@@ -2,6 +2,7 @@
 package com.medicalappointments.medicalappointmentsmanager.dtos.response;
 
 import com.medicalappointments.medicalappointmentsmanager.enums.DoctorSpecialty;
+import com.medicalappointments.medicalappointmentsmanager.enums.Shift;
 import com.medicalappointments.medicalappointmentsmanager.models.Doctor;
 import java.util.UUID;
 
@@ -9,12 +10,14 @@ public class DoctorMinResponse {
     private final UUID id;
     private final String name;
     private final String lastname;
+    private final Shift shiftType;
     private final DoctorSpecialty specialty;
     
     public DoctorMinResponse(Doctor doctor){
         this.id = doctor.getId();
         this.name = doctor.getName();
         this.lastname = doctor.getLastname();
+        this.shiftType = doctor.getShiftType();
         this.specialty = doctor.getSpecialty();
     }
 
@@ -29,6 +32,10 @@ public class DoctorMinResponse {
     public String getLastname() {
         return lastname;
     }
+
+    public Shift getShiftType() {
+        return shiftType;
+    }     
 
     public DoctorSpecialty getSpecialty() {
         return specialty;
