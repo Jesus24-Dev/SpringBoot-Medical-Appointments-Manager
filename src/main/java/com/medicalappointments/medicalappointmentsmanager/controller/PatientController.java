31f5c25f5992e42d839aad51a09b5d95626d3e69
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-20 18:25:29 -0400

    Enhance PatientController to retrieve and display patient appointments

--- src/main/java/com/medicalappointments/medicalappointmentsmanager/controller/PatientController.java
+++ src/main/java/com/medicalappointments/medicalappointmentsmanager/controller/PatientController.java
@@ -1,6 +1,10 @@
 
 package com.medicalappointments.medicalappointmentsmanager.controller;
 
+import com.medicalappointments.medicalappointmentsmanager.models.Patient;
+import com.medicalappointments.medicalappointmentsmanager.services.PatientService;
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/patient") 
 public class PatientController {
+    
+    @Autowired
+    private PatientService patientService;
 
     @GetMapping("/myAppointments")
-    public String myAppointments(Model model) {
+    public String myAppointments(Principal principal, Model model) {
+        Patient patient = patientService.getPatientByEmail(principal.getName());
+        model.addAttribute("appointments", patient.getPatientAppointments());
         return "myAppointments";
     }
 }
